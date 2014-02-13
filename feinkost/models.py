236f31fa2ee86e6d009a822c3f2534817d9e729f
Leon Handreke <leonh@ndreke.de>
2014-02-13 13:01:18 +0100

    Add inventory list with unit rendering

--- feinkost/models.py
+++ feinkost/models.py
@@ -2,14 +2,40 @@ import mongoengine
 
 from feinkost import app, db
 
+
+class ProductCategory(db.Document):
+    name = db.StringField(required=True, unique=True)
+    unit = db.StringField()
+    category = db.ReferenceField('ProductCategory', reverse_delete_rule=mongoengine.DENY)
+
+    def get_unit(self):
+        """
+        Returns:
+            str, the unit of measurement that this product category should be counted in.
+        """
+        current_category = self
+        while not current_category.unit and current_category.category:
+            current_category = current_category.category
+        return current_category.unit
+
+
 class Product(db.Document):
-    gtin = db.StringField()
+    barcode = db.StringField()
     name = db.StringField()
-    quantity = db.StringField()
+    # Quantity that is sold in the store
+    quantity = db.DecimalField(required=True)
     best_before_days = db.IntField()
+    category = db.ReferenceField(ProductCategory, reverse_delete_rule=mongoengine.DENY)
+
+    def get_unit(self):
+        return self.category.get_unit()
+    unit = property(get_unit)
+
 
 class InventoryItem(db.Document):
-    product = db.ReferenceField(Product, reverse_delete_rule=mongoengine.DENY)
+    product = db.ReferenceField(Product, reverse_delete_rule=mongoengine.DENY, required=True)
     best_before = db.DateTimeField()
-    quantity = db.StringField()
+    quantity = db.DecimalField(required=True)
 
+    def get_unit(self):
+      return self.product.get_unit()
--- feinkost/views.py
+++ feinkost/views.py
@@ -1,7 +1,51 @@
+import decimal
+
 from flask import render_template
 
 from feinkost import app
+from feinkost.models import InventoryItem
 
 @app.route('/')
 def inventoryitem_list():
-    return render_template('inventoryitem_list.html')
+    items = []
+    for item in InventoryItem.objects:
+        items.append({
+            'title': item.product.name,
+            'quantity': item.quantity,
+            'unit': item.get_unit(),
+            'best_before': item.best_before
+            })
+
+    return render_template('inventoryitem_list.html', inventory_items=items)
+
+
+UNIT_CONVERSIONS = {
+    ('ml', 'l'): lambda x: x / 1000,
+    ('l', 'ml'): lambda x: x * 1000,
+    ('g', 'kg'): lambda x: x / 1000,
+    ('kg', 'g'): lambda x: x * 1000
+}
+
+@app.template_filter('render_quantity')
+def render_quantity_filter(q):
+    quantity, unit = q
+
+    if not unit:
+        return str(quantity)
+
+    new_unit = unit
+    if unit == 'l' and quantity < 1:
+        new_unit = 'ml'
+    if unit == 'ml' and quantity > 1000:
+        new_unit = 'l'
+    if unit == 'kg' and quantity < 1:
+        new_unit = 'g'
+    if unit == 'g' and quantity > 1000:
+        new_unit = 'kg'
+
+    if new_unit != unit:
+        new_quantity = UNIT_CONVERSIONS.get((unit, new_unit))(quantity)
+    else:
+        new_quantity = quantity
+
+    return str(new_quantity.quantize(decimal.Decimal('1.'), rounding=decimal.ROUND_DOWN)) + new_unit
