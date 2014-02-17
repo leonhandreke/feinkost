b25bc6352b889883bc20b9138e90b33d8236c183
Leon Handreke <leonh@ndreke.de>
2014-02-17 08:18:35 +0100

    Add a button to remove by barcode and only show on mobile devices
    Only mobile devices have a camera and can support barcode-driven
    removal. On larger devices, the "Remove" buttons next to the inventory
    items should be used.

--- feinkost/__init__.py
+++ feinkost/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.mobility import Mobility
 
 import settings
 
@@ -8,6 +9,7 @@ app = Flask(__name__)
 app.config.from_object(settings)
 
 Bootstrap(app)
+Mobility(app)
 
 db = MongoEngine(app)
 
