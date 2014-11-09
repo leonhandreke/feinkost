053ed845d9e212a58fa365a8286bfe2b8a6a791a
Leon Handreke <leonh@ndreke.de>
2014-11-09 14:10:42 +0100

    Auto generate identity conversions in constants.UNIT_CONVERSIONS

--- feinkost/constants.py
+++ feinkost/constants.py
@@ -8,17 +8,19 @@ UNIT_KILOGRAM  = 'kg'
 UNIT_NONE = ''
 
 UNITS = [UNIT_KILOGRAM, UNIT_GRAM,
-         UNIT_LITER, UNIT_MILLILITER]
+         UNIT_LITER, UNIT_MILLILITER,
+         UNIT_NONE]
 
 DATABASE_UNITS = [UNIT_GRAM, UNIT_LITER, UNIT_NONE]
 
 UNIT_CONVERSIONS = {
-    (UNIT_LITER, UNIT_LITER): lambda x: x,
-    (UNIT_GRAM, UNIT_GRAM): lambda x: x,
-    (UNIT_NONE, UNIT_NONE): lambda x: x,
-    (UNIT_LITER, UNIT_MILLILITER): lambda x: x * 1000,
-    (UNIT_MILLILITER, UNIT_LITER): lambda x: x / 1000,
     (UNIT_KILOGRAM, UNIT_GRAM): lambda x: x * 1000,
     (UNIT_GRAM, UNIT_KILOGRAM): lambda x: x / 1000,
+
+    (UNIT_LITER, UNIT_MILLILITER): lambda x: x * 1000,
+    (UNIT_MILLILITER, UNIT_LITER): lambda x: x / 1000,
 }
+
+for u in UNITS:
+    UNIT_CONVERSIONS[(u, u)] = lambda x: x
 
